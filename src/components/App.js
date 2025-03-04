c579dc6315bc77ce6e1e76580c70895245cfad11
satyamjha <satyamjhaactor07@gmail.com>
2025-03-04 11:17:57 +0530

    Done Project

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,69 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [fields, setFields] = useState([{ id: Date.now() , name: "", age: "" }]);
+
+  const handleChange = (id, event) => {
+    const { name, value } = event.target;
+    setFields((prevFields) =>
+      prevFields.map((field) =>
+        field.id === id ? { ...field, [name]: value } : field
+      )
+    );
+  };
+
+  const addField = () => {
+    setFields([...fields, { id: Date.now(), name: "", age: "" }]);
+  };
+
+  const removeField = (id) => {
+    setFields(fields.filter((field) => field.id !== id));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Submitted Data:", fields);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        <h2>Dynamic Fields Form</h2>
+        <form onSubmit={handleSubmit}>
+          {fields.map((field) => (
+            <div key={field.id}>
+              <input 
+                type="text"
+                name="name"
+                placeholder="Name"
+                value={fields.name}
+                onChange={(e) => handleChange(field.id , e)}
+                required
+              />
+              <input 
+                type="number"
+                name="age"
+                placeholder="Age"
+                value={fields.age}
+                onChange={(e) => handleChange(fields.id , e)}
+                required
+              />
+              <button type="button" onClick={() => removeField(field.id)}>
+                Remove
+              </button>
+
+              
+
+            </div>
+          ))}
+              <button type="button" onClick={addField}>
+                    Add Fields
+              </button>
+              <button type="submit">  Submit </button>
+          
+        </form>
     </div>
   )
 }
